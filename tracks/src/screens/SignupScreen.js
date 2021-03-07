2c1f7822c6c7d8cd66c24e2e47a3276c5a5cd168
Calebe Machado <calebe.machado@dextra-sw.com>
2021-03-07 18:35:04 -0300

    add some style and authContext

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  View, StyleSheet, Text, Button,
+  View, StyleSheet,
 } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-const SignupScreen = ({ navigation }) => (
-  <>
-    <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+  },
+});
 
-    <Button
-      title="Go to Signin"
-      onPress={() => navigation.navigate('Signin')}
-    />
-    <Button
-      title="Go to main Flow"
-      onPress={() => navigation.navigate('mainFlow')}
-    />
-  </>
-);
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const styles = StyleSheet.create({});
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => ({
+  headerShown: false,
+});
 
 export default SignupScreen;
